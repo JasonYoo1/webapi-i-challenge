cd4106c49affe95547ded1bd9e2545a36d4f823e
JasonYoo1 <jyoojs@gmail.com>
2019-09-02 23:56:46 -0700

    changed from hubs to db

--- index.js
+++ index.js
@@ -1,10 +1,21 @@
 // implement your API here
 const express = require('express');
 
+//use Hubs to get access from the DB
+const Hubs = require ('./data/db.js');
+
 const server = express();
 
 server.get('/', (req, res)=>{
     res.send ('hello world')
+})
+
+server.get('/hubs', (req, res)=>{
+    Hubs.find().then(hubs=>{
+        res.status(200).json(hubs)
+    }).catch(error =>{
+        res.status(500).json({message: 'error '})
+    })
 })
 
 const port = 8000;
